6df1a228aeaaaf277bd463949146d67b2049f367
Anthony <Anteemony@gmail.com>
2022-11-10 12:51:31 +0100

    suspended user no login

--- membership/members/utils.py
+++ membership/members/utils.py
@@ -3,11 +3,20 @@ from functools import wraps
 from flask_login import current_user, logout_user
 
 
+
+#this exists to allow only users login
 def user_role_required(func):
     @wraps(func)
     def decorated_view(*args, **kwargs):
       if current_user.is_authenticated:
         if current_user.role == "USER":
+
+          #suspended users can not log in
+          if current_user._is_suspended:
+            flash("Your account has been suspended and can therefore not log in")
+            return redirect(url_for('members.login'))
+
+          #logic for users to first fill their form before accessing other pages
           if current_user.business_name == None and request.endpoint != 'members.edit_business_profile' and request.method!="POST":
             flash("You must first complete this form before you can proceed")
             return redirect(url_for('members.edit_business_profile'))
